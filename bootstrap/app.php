22398a8e96a380b82b5d4d210f4f55e652956648
SUBxZERO <bangboy892005@gmail.com>
2025-12-28 13:23:16 +0700

    w45y

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,9 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
+        
+        // Trust Render Load Balancer
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
